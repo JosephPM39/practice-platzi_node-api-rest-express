44a64c607610cbf4cee8d7ca8c5174627e9af248
JosephPM39 <josias.jjpm49@hotmail.com>
2022-01-25 06:13:35 -0600

    curso finalizado

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middleware/error.handle');
 
 const app = express();
 const port = 3000;
+
 app.use(express.json());
+
+const whitelist = ['http://localhost:5500', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
