b10ef102aaeb32c7f937c018a42abf5996edec7e
JosephPM39 <josias.jjpm49@hotmail.com>
2022-01-29 11:00:46 -0600

    Probando pools en postgressql

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,11 +1,14 @@
 const faker = require('faker');
 const boom = require('@hapi/boom');
+const pool = require('../libs/postgres.pool');
 
 class ProductService {
 
-  constructor() {
+  constructor(){
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err) => console.error(err));
   }
 
   async generate() {
@@ -30,13 +33,10 @@ class ProductService {
     return newProduct;
   }
 
-  //Emunlando una consulta que tarde hacia la base de datos
-  find() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(this.products);
-      }, 5000);
-    });
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const rta = await this.pool.query(query);
+    return rta.rows;
   }
 
   async findOne(id) {
